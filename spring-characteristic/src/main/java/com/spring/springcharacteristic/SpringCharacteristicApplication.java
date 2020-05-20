70d8acb42907318ce180276997db7e02aec5ce7d
keweizhou <1514991931@qq.com>
2020-05-20 21:52:39 +0800

    init repository

---
+++ spring-characteristic/src/main/java/com/spring/springcharacteristic/SpringCharacteristicApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springcharacteristic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCharacteristicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCharacteristicApplication.class, args);
+    }
+
+}
