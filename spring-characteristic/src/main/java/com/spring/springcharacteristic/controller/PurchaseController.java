4ec41e3dd56105110053636dd26536fff6b943bf
keweizhou <1514991931@qq.com>
2020-05-24 11:04:09 +0800

    add Spring Log AOP

--- spring-characteristic/src/main/java/com/spring/springcharacteristic/controller/PurchaseController.java
+++ spring-characteristic/src/main/java/com/spring/springcharacteristic/controller/PurchaseController.java
@@ -1,21 +1,26 @@
 package com.spring.springcharacteristic.controller;
 
+import com.spring.springcharacteristic.aop.LogAnnotation;
 import com.spring.springcharacteristic.aop.Purchase;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Description
  * Date 2020/5/24 0:15
  * Created by kwz
  */
-@Controller
+@RestController("purchase")
 public class PurchaseController {
 
     @Setter(onMethod_ = @Autowired)
     private Purchase purchase;
 
+    @GetMapping()
+    @LogAnnotation
     public String testBuy() {
         return purchase.buy();
     }
