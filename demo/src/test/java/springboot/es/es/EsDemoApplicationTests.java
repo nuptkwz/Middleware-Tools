abc41b6e98f0bf0a1abd071c26031933e898232e
keweizhou <1514991931@qq.com>
2020-09-14 22:56:41 +0800

    add es demo

---
+++ demo/src/test/java/springboot/es/es/EsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.es.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
